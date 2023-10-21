d10abbbc397f2e2144b971127a777afd7b3b52f2
Ing Brayan Martinez <brayanmartinez827@gmail.com>
2023-10-21 18:26:38 -0400

    optimizar algoritmos

--- lib/dinamic/fibonacci.dart
+++ lib/dinamic/fibonacci.dart
@@ -1,10 +1,8 @@
 int fibonacci(int n) {
-  /* If the input is 0 or 1 just return the same
-       This will set the first 2 values of the sequence */
+
   if (n <= 1) {
     return n;
   }
 
-  /* Add the last 2 values of the sequence to get next */
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
