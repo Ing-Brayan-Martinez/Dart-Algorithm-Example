c8781bacad9ee86c9e6abdc944fabc9c58e00622
Ing Brayan Martinez <brayanmartinez827@gmail.com>
2024-06-28 19:02:42 -0400

    limpiar codigo

--- lib/dynamic/factorial.dart
+++ lib/dynamic/factorial.dart
@@ -1,3 +1,6 @@
 int factorial(int n) {
-  return switch (n) { 0 => 1, _ => n * factorial(n - 1) };
+  return switch (n) {
+    0 => 1,
+    _ => n * factorial(n - 1),
+  };
 }
--- lib/dynamic/fibonacci.dart
+++ lib/dynamic/fibonacci.dart
@@ -2,6 +2,6 @@ int fibonacci(int n) {
   return switch (n) {
     0 => 0,
     1 => 1,
-    _ => fibonacci(n - 1) + fibonacci(n - 2)
+    _ => fibonacci(n - 1) + fibonacci(n - 2),
   };
 }
