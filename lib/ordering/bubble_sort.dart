4a519fc8daabfa900b9f39dd9a826dedb4b63a3f
Ing Brayan Martinez <brayanmartinez827@gmail.com>
2024-06-30 19:37:52 -0400

    mejorar version

--- lib/ordering/bubble_sort.dart
+++ lib/ordering/bubble_sort.dart
@@ -1,14 +1,14 @@
 void bubbleSort(List<int> data) {
-  var swap_check = true;
-  var n = data.length;
-
-  for (var i = 0; (i < n) && (swap_check); i++) {
-    swap_check = false;
-    for (var j = 0; j < n - 1 - i; j++) {
+  for (var i = 0; i < data.length - 1; i++) {
+    var breakCount = 0;
+    for (var j = 0; j < data.length - i - 1; j++) {
       if (data[j] > data[j + 1]) {
-        swap_check = true;
         _swap(j, j + 1, data);
+        breakCount++;
       }
+    }
+    if (breakCount == 0) {
+      break;
     }
   }
 }
@@ -16,7 +16,7 @@ void bubbleSort(List<int> data) {
 void _swap(int first, int second, List<int> data) {
   var value1 = data[first];
   var value2 = data[second];
-  var temp = value1;
+  final temp = value1;
   value1 = value2;
   value2 = temp;
   data[first] = value1;
