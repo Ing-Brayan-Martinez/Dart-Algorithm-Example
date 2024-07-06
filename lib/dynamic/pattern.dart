957096ac17f59bca905e141c41e02457c4b900ab
Ing Brayan Martinez <brayanmartinez827@gmail.com>
2024-07-06 13:02:12 -0400

    mejorar version

---
+++ lib/dynamic/pattern.dart
@@ -0,0 +1,87 @@
+
+// root element
+abstract class Element {
+  String get();
+}
+
+
+// html element
+abstract class HtmlElement extends Element {
+
+}
+
+final class Div extends HtmlElement {
+  String get() {
+    return "";
+  }
+}
+
+final class Span extends HtmlElement {
+  String get() {
+    return "";
+  }
+}
+
+final class Paragraph extends HtmlElement {
+  String get() {
+    return "";
+  }
+}
+
+final class Image extends HtmlElement {
+  String get() {
+    return "";
+  }
+}
+
+
+// internal element
+abstract class InternalElement extends Element {
+
+}
+
+final class Text extends InternalElement {
+  String get() {
+    return "";
+  }
+}
+
+final class HtmlLiteral extends InternalElement {
+  String get() {
+    return "";
+  }
+}
+
+final class Nothing extends InternalElement {
+  String get() {
+    return "";
+  }
+}
+
+
+// custom element
+abstract class CustomElement extends Element {
+
+}
+
+
+
+String getElement(Element element) {
+  return switch (element) {
+    HtmlElement html => switch (html) {
+      Div div => '<div>' + div.get() + '</div>',
+      Span span => '<span>' + span.get() + '</span>',
+      Paragraph paragraph => '<p>' + paragraph.get() + '</p>',
+      Image image => '<img src="' + image.get() + '" />',
+      _ => throw UnimplementedError(),
+    },
+    InternalElement internal => switch (internal) {
+      Text text => text.get(),
+      HtmlLiteral htmlLiteral => htmlLiteral.get(),
+      Nothing nothing => nothing.get(),
+      _ => throw UnimplementedError(),
+    },
+    CustomElement custom => custom.toString(),
+    _ => throw UnimplementedError(),
+  };
+}
---
+++ test/dynamic/pattern_test.dart
@@ -0,0 +1,49 @@
+import 'package:dart_algorithm_example/dynamic/pattern.dart';
+import 'package:test/test.dart';
+
+void main() {
+  //unit test
+  test('pattern()', () {
+    final element = Div();
+
+    print('-- Pattern -- \n\n');
+
+    if (element is Div) {
+      print('div');
+    }
+
+    if (element is HtmlElement) {
+      print('html element');
+    }
+
+    if (element is Element) {
+      print('element');
+    }
+
+    print(getElement(element) + '\n\n');
+
+    print('Test Passed!');
+  });
+
+  test('pattern2()', () {
+    final Object value = 'maria';
+
+    print('-- Pattern 2 -- \n\n');
+
+    if (value is String) {
+      print(value.toUpperCase());
+    }
+
+    if (value is int) {
+      print(value.round());
+    }
+
+    if (value is double) {
+      print(value.ceil());
+    }
+
+    print('\n\n');
+
+    print('Test Passed!');
+  });
+}
